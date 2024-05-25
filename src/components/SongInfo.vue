6c769a2e73c986405504d9b1bc89c358a2cb49aa
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-05-25 15:00:33 -0600

    add placeholder to playlist songs loading

--- src/components/SongInfo.vue
+++ src/components/SongInfo.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="playlistSong" v-intersect="onIntersect">
+    <div v-if="!songInfo.albumArt" class="image-placeholder"></div>
     <img :src="songInfo.albumArt" alt="Album Art" v-if="songInfo.albumArt" />
     <div class="playlistText">
-      <p><strong>Title:</strong> {{ songInfo.title }}</p>
-      <p><strong>Artist:</strong> {{ songInfo.artist }}</p>
-      <p><strong>Album:</strong> {{ songInfo.album }}</p>
-      <!-- <p><strong>Duration:</strong> {{ songInfo.duration }}</p> -->
+      <p v-if="!isIntersected"><strong>Title:</strong> <span class="placeholder"></span></p>
+      <p v-else><strong>Title:</strong> {{ songInfo.title }}</p>
+      <p v-if="!isIntersected"><strong>Artist:</strong> <span class="placeholder"></span></p>
+      <p v-else><strong>Artist:</strong> {{ songInfo.artist }}</p>
+      <p v-if="!isIntersected"><strong>Album:</strong> <span class="placeholder"></span></p>
+      <p v-else><strong>Album:</strong> {{ songInfo.album }}</p>
+      <!-- <p v-if="!isIntersected"><strong>Duration:</strong> <span class="placeholder"></span></p>
+      <p v-else><strong>Duration:</strong> {{ songInfo.duration }}</p> -->
     </div>
   </div>
 </template>
@@ -56,3 +61,29 @@ export default {
 };
 </script>
 
+<style scoped>
+.playlistText p {
+  margin: 0;
+}
+
+.placeholder {
+  display: inline-block;
+  width: 100px;
+  height: 1em;
+  background-color: #e0e0e0;
+  border-radius: 4px;
+  animation: pulse 1.5s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    background-color: #e0e0e0;
+  }
+  50% {
+    background-color: #f0f0f0;
+  }
+  100% {
+    background-color: #e0e0e0;
+  }
+}
+</style>
