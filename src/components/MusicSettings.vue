19bb54530bf8a12d68f62d3cf1207c2cb50d0251
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-05-25 12:26:40 -0600

    fixed CSS for firefox

--- src/components/MusicSettings.vue
+++ src/components/MusicSettings.vue
@@ -120,6 +120,39 @@ input:checked + .slider:before {
   transform: translateX(20px);
 }
 
+input[type=range] {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 6px;
+  background: #ddd;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+input[type=range]:hover {
+  opacity: 1;
+}
+
+input[type=range]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  background: #2196F3;
+  cursor: pointer;
+  border-radius: 50%;
+}
+
+input[type=range]::-moz-range-thumb {
+  width: 20px;
+  height: 20px;
+  background: #2196F3;
+  cursor: pointer;
+  border-radius: 50%;
+}
+
 .dropdown-enter-active, .dropdown-leave-active {
   transition: opacity 0.3s;
 }
@@ -128,4 +161,3 @@ input:checked + .slider:before {
   opacity: 0;
 }
 </style>
-
